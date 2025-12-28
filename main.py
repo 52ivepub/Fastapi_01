6223648f59a985b34ac4aafa37722cda3878bb2d
Евгений Бусыгин (ПК) <abusyginza@gmail.com>
2025-12-28 12:53:22 +0700

    step_3 crate database, time: 32.00

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import uvicorn
-from pydantic import EmailStr, BaseModel
+from core.models import Base, db_helper
 from items_views import router as items_router
 from users.views import router as users_router
 
-app = FastAPI()
-app.include_router(items_router,)
-app.include_router(users_router,)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    
 
 
+app = FastAPI(lifespan=lifespan)
+app.include_router(items_router)
+app.include_router(users_router)
 
 
 @app.get("/")
@@ -26,16 +34,14 @@ def hello(name: str):
     }
 
 
-
 @app.post("/calc/add")
 def add(a: int, b: int):
     return {
         "a": a,
         "b": b,
-        "result": a +b,
+        "result": a + b,
     }
 
 
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
