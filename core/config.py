74e5da183fbaa70a7bf82126bf09cd2147de7976
Евгений Бусыгни (Ноутбук) <abusyginza@gmail.com>
2026-02-05 11:00:48 +0700

    Refresh JWT FastAPI | Обновление access токена при помощи refresh токена, 15.00, 03.02.2026

--- core/config.py
+++ core/config.py
@@ -14,6 +14,7 @@ class AuthJWT(BaseModel):
     public_key_path: Path = "/home/user/Документы/Fastapi_01/certs/jwt-public.pem"
     algorithm: str = "RS256"
     access_token_expire_minutes: int = 15
+    refresh_token_expire_days: int = 30
 
 
 class Setting(BaseSettings):
